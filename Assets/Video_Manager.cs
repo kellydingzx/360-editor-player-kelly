e43b8ca5201293942c22ad4993942d62fb69eace
kellydingzx <55750344+kellydingzx@users.noreply.github.com>
2020-12-17 10:12:56 +0800

    first

---
+++ Assets/Video_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video_Manager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+}
